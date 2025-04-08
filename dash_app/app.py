216f3007ae4f55d08fbbeecd52d4e61ff28567d4
jiyue1214 <jiyue1214@gmail.com>
2025-04-08 22:14:45 +0100

    Update app.py

--- dash_app/app.py
+++ dash_app/app.py
@@ -56,5 +56,4 @@ app.layout = dbc.Container([
 # Callback to fetch the data
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))  # 8050 is just a default fallback
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True)
